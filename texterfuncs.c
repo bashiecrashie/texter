0ef45f2caccae3b16c1341f602debadb30a34d2a
bashiecrashie <dmitriydanilovdanilov2016@gmail.com>
2023-05-11 03:18:40 +0300

    COUT works

--- texterfuncs.c
+++ texterfuncs.c
@@ -1,86 +1,77 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include"stringfuncs.h"
 
-/*FILE *retfp(const char *file)
-{
-
-    FILE *FilePointer = fopen(file, "r");
-
-    return FilePointer;
-
-}*/
-
-void quotesclear(FILE *fin, FILE *fout)
-{
-
-    int ch = 0;
-
-    while(ch != EOF)
-    {
-        ch = fgetc(fin);
-        if(ch != '"')
-            fputc(ch, fout);
-    }
-
-
-}
-
-void commasclear(FILE *fin, FILE *fout)
-{
-
-    int ch = 0;
-
-    while(ch != EOF)
-    {
-        ch = fgetc(fin);
-        if(ch != ',')
-            fputc(ch, fout);
-
-    }
-
-
-}
-
-int sizeoffile(FILE *file)
-{
-
-    int ch = 0, size = 0;
-
-    while(ch != EOF)
-    {
-        ch = fgetc(file);
-        ++size;
-    }
-
-    return size;
-
-}
+#define MAX_LINE_LENGTH 512
+#define COLUMN_WIDTH 20
+#define C_FILE_BEGIN "#include<stdio.h>\nint main()\n{\nchar *content=\""
+#define C_FILE_END "\";\nprintf(\"%s\", content);\nreturn 0;\n}"
 
 void cout(const char *in, const char *out)
 {
 
-    int *GetPointer = NULL, ch = 0;
-
-    char *str, *clrstr, *c_file_begin = "#include<stdio.h>\nint main()\n{\nchar *content=\"", *c_file_end = "\";\nprintf(\"%s\", content);\nreturn 0;\n}";
+    int ch = 0;
 
     FILE *FileInput = fopen(in, "r"), *FileOutput = fopen(out, "w");
 
-    int size = sizeoffile(FileInput), counter = 0;
+    fputs(C_FILE_BEGIN, FileOutput);
 
-    char content[size];
+   while((ch = fgetc(FileInput)) != EOF)
+   {
+        
+        if(ch == 34)
+            ch = 1;
 
-    fputs(c_file_begin, FileOutput);
+        if(ch == '\n')
+        {
+            ch = 34;
+            fputs("\\n", FileOutput);
+            fputc(ch, FileOutput);
+            fputc('\n', FileOutput);
+        }
 
-    /*:-|*/
+        fputc(ch, FileOutput);
+        
+   }
 
-    fputs(c_file_end, FileOutput);
+   fputs(C_FILE_END, FileOutput);
 
     fclose(FileOutput);
+
+    fclose(FileInput);
 }
 
 void txtout(const char *in, const char *out)
 {
-    ;
+
+    FILE *InputFile = fopen(in, "r");
+    FILE *OutputFile = fopen(out, "w");
+
+    int counter = 1, ch = 0, columns = colnum(in);
+
+    const int BUFF_SIZE = 512;
+
+    char word[BUFF_SIZE];
+
+    char *clrword;
+
+    while((fscanf(InputFile, "%s", word)) != EOF)
+    {
+
+        if(counter == columns)
+            fputs("\n", OutputFile);
+
+        
+        fprintf(OutputFile, "%s", word);
+        ++counter;
+           
+
+    }
+
+    fclose(InputFile);
+    fclose(OutputFile);
+
 }
 
 void htmlout(const char *in, const char *out)
